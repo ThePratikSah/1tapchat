e19e9d74b24526fc4518f1f382ccdacd7241a96b
Pratik Sah <shivam3448@gmail.com>
2021-10-08 19:25:36 +0530

    removed country code and set to india only

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-function oneTapChat() { 
+function oneTapChat() {
   let number = document.getElementById("number").value;
   if (number !== "" && number.length >= 10) {
     let phoneNumber = Number(number);
-    window.location.href = `https://wa.me/${phoneNumber}?text=Hi`;
+    window.location.href = `https://wa.me/91${phoneNumber}?text=Hi`;
   } else {
     document.getElementById("element").style.display = "block";
   }
